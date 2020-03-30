cbf42db9a1892d07c89227f422fdeb571cd6a1fe
McEnos <joshua.odiwuor@turnkeyafrica.com>
2020-03-31 02:22:16 +0300

    exposing entity ids  ro be accessed by url

--- src/main/java/com/bookstore/onlinebookstore/repositories/BookCategoryRepository.java
+++ src/main/java/com/bookstore/onlinebookstore/repositories/BookCategoryRepository.java
@@ -1,9 +1,9 @@
 package com.bookstore.onlinebookstore.repositories;
 
-import com.bookstore.onlinebookstore.entities.Book;
+import com.bookstore.onlinebookstore.entities.BookCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepositories extends JpaRepository<Book, Long> {
+public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
 }
--- src/main/java/com/bookstore/onlinebookstore/repositories/BookRepository.java
+++ src/main/java/com/bookstore/onlinebookstore/repositories/BookRepository.java
@@ -1,8 +1,9 @@
 package com.bookstore.onlinebookstore.repositories;
 
+import com.bookstore.onlinebookstore.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookCategory extends JpaRepository<BookCategory, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
